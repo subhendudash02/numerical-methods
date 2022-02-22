4a1dd4d305c5754213881506a7f870eae2a1a706
Subhendu Dash <subhendu.dash2020@vitstudent.ac.in>
2022-02-23 00:11:30 +0530

    Added code of Gauss-Seidel method

--- src/components/equations.jsx
+++ src/components/equations.jsx
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
 import "../styles/equations.css";
+import gaussSeidel from '../methods/gaussSeidel';
 
 class Equations extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {a1: '', b1: '', c1: '', const1: ''};
+        this.state = {
+            // First equation
+            a1: '', 
+            b1: '', 
+            c1: '', 
+            const1: '',
+
+            // Second equation
+            a2: '', 
+            b2: '', 
+            c2: '', 
+            const2: '',
+
+            // Third equation
+            a3: '', 
+            b3: '', 
+            c3: '', 
+            const3: '',
+        };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleSubmit(event) {
-        const {a1, b1, c1, const1} = this.state;
+        const {a1, b1, c1, const1, a2, b2, c2, const2, a3, b3, c3, const3} = this.state;
+        let matrix = [[a1, b1, c1, const1], [a2, b2, c2, const2], [a3, b3, c3, const3]];
         event.preventDefault();
-        
+        gaussSeidel(matrix);
     }
 
     handleChange(event) {
@@ -31,17 +51,17 @@ class Equations extends React.Component {
                 <input name="c1" type="text" id="c1" value={this.state.c1} onChange={this.handleChange} /> z = 
                 <input name="const1" type="text" id="const1" value={this.state.const1} onChange={this.handleChange} />
         
-                {/* <h2>Second Equation</h2>
-                <input type="text" id="a2" /> x +
-                <input type="text" id="b2" /> y + 
-                <input type="text" id="c2" /> z = 
-                <input type="text" id="const2" />
-        
+                <h2>Second Equation</h2>
+                <input name="a2" type="text" id="a2" value={this.state.a2} onChange={this.handleChange} /> x +
+                <input name="b2" type="text" id="b2" value={this.state.b2} onChange={this.handleChange} /> y + 
+                <input name="c2" type="text" id="c2" value={this.state.c2} onChange={this.handleChange} /> z = 
+                <input name="const2" type="text" id="const2" value={this.state.const2} onChange={this.handleChange} />
+
                 <h2>Third Equation</h2>
-                <input type="text" id="a3" /> x +
-                <input type="text" id="b3" /> y + 
-                <input type="text" id="c3" /> z = 
-                <input type="text" id="const3" /> */}
+                <input name="a3" type="text" id="a2" value={this.state.a3} onChange={this.handleChange} /> x +
+                <input name="b3" type="text" id="b2" value={this.state.b3} onChange={this.handleChange} /> y + 
+                <input name="c3" type="text" id="c2" value={this.state.c3} onChange={this.handleChange} /> z = 
+                <input name="const3" type="text" id="const1" value={this.state.const3} onChange={this.handleChange} />
                 <button>Submit</button>
             </form>
         );
---
+++ src/methods/gaussSeidel.jsx
@@ -0,0 +1,40 @@
+import {round} from 'mathjs';
+
+function gaussSeidel(array) {
+    // matrix = [[a1, b1, c1, const1], [a2, b2, c2, const2], [a3, b3, c3, const3]]
+
+    let output = ``;
+
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array[0].length; j++) {
+            array[i][j] = parseInt(array[i][j]);
+        }
+    }
+
+    let x = (y1, z1) => {return ((array[0][3] - (array[0][1] * y1) - (array[0][2] * z1)) / array[0][0])};
+    let y = (x1, z1) => {return ((array[1][3] - (array[1][0] * x1) - (array[1][2] * z1)) / array[1][1])};
+    let z = (x1, y1) => {return ((array[2][3] - (array[2][0] * x1) - (array[2][1] * y1)) / array[2][2])};
+
+    let x_up = x(0, 0);
+    let y_up = y(x_up, 0);
+    let z_up = z(x_up, y_up);
+
+    output += `Iteration - 1: <br>x = ${round(x_up, 4)}<br>
+                                y = ${round(y_up, 4)}<br>
+                                z = ${round(z_up, 4)}<br><br>`;
+    
+    for (let i = 2; i <= 5; i++) {
+            x_up = x(y_up, z_up);
+            y_up = y(x_up, z_up);
+            z_up = z(x_up, y_up);
+
+            output += `Iteration - ${i}: <br>x = ${round(x_up, 4)}<br>
+                            y = ${round(y_up, 4)}<br>
+                            z = ${round(z_up, 4)}<br><br>`;
+    }
+
+    console.log(output);
+
+}
+
+export default gaussSeidel; 
